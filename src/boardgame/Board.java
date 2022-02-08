722098841f86d25210f2a2f98aaf99280fbb168f
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-02-08 00:08:40 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public class Board {
+	
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
@@ -13,24 +14,29 @@ public class Board {
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
+	
 	public int getRows() {
 		return rows;
 	}
+	
 	public int getColumns() {
 		return columns;
 	}
+	
 	public Piece piece(int row, int column) {
 		if(!positionExists(row,column)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[row][column];
 	}
+	
 	public Piece piece(Position position) {
 		if(!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
 	public void placePiece(Piece piece, Position position){
 		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position "+position);
@@ -38,6 +44,20 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position=position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row>=0 && row< rows && column>=0 && column<columns;		
 	}
